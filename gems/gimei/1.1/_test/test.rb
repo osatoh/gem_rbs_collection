392644f34c44956b569be3824d6c9a0bfa9cccbb
osatoh <sugar961216@gmail.com>
2023-03-08 01:32:02 +0900

    Add gimei type

---
+++ gems/gimei/1.1/_test/test.rb
@@ -0,0 +1,29 @@
+require "gimei"
+
+gimei = Gimei.name
+gimei.kanji
+gimei.hiragana
+gimei.katakana
+gimei.romaji
+gimei.gender
+gimei.male?
+gimei.female?
+
+gimei = Gimei.male
+gimei.male?
+gimei.female?
+gimei.gender
+gimei.kanji
+
+gimei = Gimei.female
+gimei.male?
+gimei.female?
+gimei.gender
+gimei.kanji
+
+Gimei.kanji
+Gimei.hiragana
+Gimei.katakana
+Gimei.romaji
+
+Gimei.config.rng = Random.new(42)
